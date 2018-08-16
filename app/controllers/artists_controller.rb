9191455c2b2166007a155f6d2d3aaf28884f8029
Jed Peek <jedpeek@yahoo.com>
2018-08-16 21:28:01 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,6 +12,11 @@ class ArtistsController < ApplicationController
     @artist = Artist.create(artist_params)
   end
 
+  def create
+    @artist = Artist.create(artist_params)
+  end
+
+
   private
 
   def artist_params
