22093567939dd921313a24d1d4f675414e2cd4d7
Jed Peek <jedpeek@yahoo.com>
2018-08-16 21:31:12 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -17,6 +17,18 @@ class ArtistsController < ApplicationController
     redirect_to artist_url(@artist)
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    @artist.save
+
+    redirect_to artist_url(@artist)
+
+
 
   private
 
